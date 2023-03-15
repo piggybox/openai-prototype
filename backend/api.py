02bf64f07f80c898de43f86d5859b9d28d6d99d9
Sky Yin <syin@meta.com>
2023-03-15 14:32:45 -0700

    [feat] add input validation

--- backend/api.py
+++ backend/api.py
@@ -1,15 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from ai import generate_branding_snippet, generate_branding_keywords
 
 app = FastAPI()
 
+
 @app.get("/snippet")
 async def generate_snippet(prompt: str):
+    validate_input(prompt)
     snippet = generate_branding_snippet(prompt)
-    return {"snippet": snippet}
+    return {"snippet": snippet, "keywords": []}
 
 
 @app.get("/keywords")
 async def generate_keywords(prompt: str):
+    validate_input(prompt)
     keywords = list(generate_branding_keywords(prompt))
-    return {"keywords": keywords}
+    return {"snippet": None, "keywords": keywords}
+
+
+@app.get("/snippet_and_keywords")
+async def generate_keywords(prompt: str):
+    validate_input(prompt)
+    snippet = generate_branding_snippet(prompt)
+    keywords = list(generate_branding_keywords(prompt))
+    return {"snippet": snippet, "keywords": keywords}
+
+MAX_INPUT_LENGTH = 32
+
+
+def validate_input(prompt: str):
+    if len(prompt) >= MAX_INPUT_LENGTH:
+        raise HTTPException(status_code=400, detail="Input length is too long")
