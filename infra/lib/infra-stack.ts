8cf46c74ab55acded0a8f53dd0ce515b57fb0b57
Sky Yin <sky.yin@gmail.com>
2023-03-15 18:14:47 -0700

    [feat] add base layer for lambda

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,22 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib"
+import { Construct } from "constructs"
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as lambda from "aws-cdk-lib/aws-lambda"
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
+
+    const layer = new lambda.LayerVersion(this, "BaseLayer", {
+      code: lambda.Code.fromAsset("base/layer.zip"),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_9],
+    })
 
     const apiLambda = new lambda.Function(this, "ApiFunction", {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset("../backend/"),
-      handler: "api.handler"
+      handler: "api.handler",
+      layers: [layer],
     })
   }
 }
